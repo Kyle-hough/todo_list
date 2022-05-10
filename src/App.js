ad6edb6de3b563d95f15c33eac7c8608869cda06
Kyle-hough <kyle.hough05@gmailcom>
2022-05-09 21:16:00 -0600

    First comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    }
+
+    setTodos([...todos, todoItem])
+    setNewTodo("")
+  }
+  const handleDelete = (delIdx) => {
+    const filteredTodos =todos.filter((todo, i) => {
+      return i !== delIdx;
+    })
+    setTodos(filteredTodos)
+  }
+  const handleComplete = (idx) => {
+    const updatedTodos = todos.map((todo,i) => {
+      if (idx === i) {
+        todo.complete = !todo.complete;
+      }
+      return todo;
+    })
+    setTodos(updatedTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" name="newTodo" value={newTodo} 
+        onChange={e=>setNewTodo(e.target.value)}/>
+        <button type="submit" style={{marginLeft: "10px"}}>Add</button>
+      </form>
+
+      {
+        todos.map((todo, i) => {
+          const todoClasses = [];
+
+          if(todo.complete) {
+            todoClasses.push("strikethrough");
+          }
+          return( 
+          <div key={i} >
+            <span className={todoClasses.join(" ")} > {todo.text}</span>
+            <input onChange={(e)=>{handleComplete(i)}} checked={todo.complete} type="checkbox" />
+            <button onClick={(e) =>handleDelete(i)} style={{marginLeft: "10px"}}>Delete</button>
+          </div>
+          )
+        })
+      }
     </div>
   );
 }
